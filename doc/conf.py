7d211dea6cacb939de44d20f0b097c1abfcede25
Christoph Wagner <christoph.wagner@tu-ilmenau.de>
2018-02-06 17:59:30 +0100

    Modify doc/conf.py
    Append relative paths to code rather than prepending it to achieve a
    preference for the locally installed verstion. This is needed in virtualenvs
    when there is no control to issue a local build first, just pip-installing.
    Workaround for readthedocs.io build trouble.

--- doc/conf.py
+++ doc/conf.py
@@ -19,9 +19,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('../util'))
-
+sys.path.append(os.path.abspath('../'))
+sys.path.append(os.path.abspath('../util'))
+print(sys.path)
 
 # -- General configuration ------------------------------------------------
 
