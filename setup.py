66433be8bc5bc607cd1adf5ecd290d205fa1827b
Christoph Wagner <christoph.wagner@tu-ilmenau.de>
2018-08-17 23:00:51 +0200

    Add Python 3.7 to CI Builds

--- setup.py
+++ setup.py
@@ -351,6 +351,7 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
             'Topic :: Scientific/Engineering',
             'Topic :: Scientific/Engineering :: Mathematics',
             'Topic :: Software Development :: Libraries'
