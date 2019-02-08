33e5e998a7f27a646e482eef10661b9d8c748a04
christoph.wagner@tu-ilmenau.de <christoph.wagner@tu-ilmenau.de>
2019-02-08 13:38:52 +0100

    Disallow zero-dim arrays in OMP

--- fastmat/algorithms/OMP.pyx
+++ fastmat/algorithms/OMP.pyx
@@ -55,7 +55,7 @@ cdef class OMP(Algorithm):
     >>> # define the sampling positions
     >>> t = np.linspace(0, 20 * np.pi, n)
     >>> # construct the convolution matrix
-    >>> c = np.cos(2 * t) * np.exp(-t ** 2)
+    >>> c = np.cos(2 * t)
     >>> C = fm.Circulant(c)
     >>> # create the ground truth
     >>> x = np.zeros(n)
@@ -132,13 +132,16 @@ cdef class OMP(Algorithm):
         #                       strides
         #
 
-        if arrB.ndim > 2:
+        if arrB.ndim > 2 or arrB.ndim < 1:
             raise ValueError("Only n x m arrays are supported for OMP")
 
         if arrB.ndim == 1:
             self.arrB = arrB.reshape((-1, 1))
         else:
             self.arrB = arrB
+
+        if self.numMaxSteps <= 0:
+            raise ValueError("OMP would like to do at least one step for you")
 
         # get the number of vectors to operate on
         self.numN, self.numM, self.numL = \
