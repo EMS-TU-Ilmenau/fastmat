c45c8eb6b65dde5ab96acb0954307e37fa5e4c17
christoph.wagner@tu-ilmenau.de <christoph.wagner@tu-ilmenau.de>
2023-04-19 14:07:59 +0200

    Add .largestEigenvalue query prior to .largestEigenVectors, which used to generate the former

--- test/interface.py
+++ test/interface.py
@@ -41,6 +41,9 @@ class TestInterface(unittest.TestCase):
 #        np.testing.assert_array_equal(np.squeeze(u1), u[:, idx_largest_sv])
 #        np.testing.assert_array_equal(np.squeeze(v1), v[idx_largest_sv, :])
         self.assertEqual(instance.largestEV, instance.largestEigenValue)
+        self.assertEqual(instance.H.largestEV, instance.largestEigenValue)
+        self.assertEqual(instance.T.largestEV, instance.largestEigenValue)
+        self.assertEqual(instance.conj.largestEV, instance.largestEigenValue)
         np.testing.assert_array_equal(
             instance.largestEigenVec, instance.largestEigenVec
         )
